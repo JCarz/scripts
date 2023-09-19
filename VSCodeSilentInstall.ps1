8f887180431c5813107c77d973a8c7b464cf0260
Jaylen Carroll <55265765+JCarz@users.noreply.github.com>
2023-09-18 23:44:21 -0400

    Add files via upload

---
+++ VSCodeSilentInstall.ps1
@@ -0,0 +1,8 @@
+﻿$LogFile = "C:\Windows\Temp\VSCode_Install.log"
+$TargetFolder = "B:\Program Files\VSCode"
+$SourceFolder = $PSScriptRoot 
+$RepoSourceFolder = $SourceFolder + '\Repo_Basic'
+$exeFile = "VSCodeSetup-x64-1.82.2.exe"
+$fullPath = $SourceFolder + $exeFile
+
+Start-Process VSCodeSetup-x64-1.82.2.exe -ArgumentList "/VERYSILENT /NORESTART /MERGETASKS=!runcode--local-package-repository=$($RepoSourceFolder) --shared=yes --package-set-basic install" -Wait -PassThru -WindowStyle Hidden
